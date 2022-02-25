a614b5dd745bb5109163ab941aa9e834a8364d4f
John Ferguson Smart <john.smart@wakaleo.com>
2022-02-25 08:40:41 +0000

    Updated sample data-driven scenario

--- src/test/java/swaglabs/features/maths/WhenDoingSums.java
+++ src/test/java/swaglabs/features/maths/WhenDoingSums.java
@@ -1,10 +1,8 @@
 package swaglabs.features.maths;
 
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
-import net.thucydides.core.annotations.SingleBrowser;
 import net.thucydides.core.annotations.Steps;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,30 +16,26 @@ class WhenDoingSums {
     @Steps
     CalculatorSteps calulate;
 
-    @Nested
-    class ABasicCalculator {
+    @BeforeEach
+    void openTheCalculator() {
+        calulate.openTheCalculatorApp();
+    }
 
-        @BeforeEach
-        void openTheCalculator() {
-            calulate.openTheCalculatorApp();
-        }
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,+,2,3",
+            "7,-,3,4",
+            "3,*,2,6",
+            "10,/,2,5",
+    })
+    void shouldCalculateResults(String a, String operator, String b, String expectedResult) {
+        String actualResult = calulate.theAnswerTo(a, operator, b);
+        assertThat("" + actualResult).isEqualTo(expectedResult);
+    }
 
-        @ParameterizedTest
-        @CsvSource({
-                "1,+,2,3",
-                "7,-,3,4",
-                "3,*,2,6",
-                "10,/,2,5",
-        })
-        void shouldCalculateResults(String a, String operator, String b, String expectedResult) {
-            String actualResult = calulate.theAnswerTo(a, operator, b);
-            assertThat("" + actualResult).isEqualTo(expectedResult);
-        }
-
-        @Test
-        void shouldReportDivisionByZero() {
-            String result = calulate.theAnswerTo("10","/","0");
-            assertThat(result).isEqualTo("Infinity");
-        }
+    @Test
+    void shouldReportDivisionByZero() {
+        String result = calulate.theAnswerTo("10", "/", "0");
+        assertThat(result).isEqualTo("Infinity");
     }
 }
